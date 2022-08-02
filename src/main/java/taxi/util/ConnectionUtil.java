d9bb3cdd9f23b1b87c5c6d29340a04c45738ad7b
Oleksii Mospan <alexey.mospan@gmail.com>
2022-08-02 22:39:52 +0300

    Configured for connecting to DB on freemysqlhosting.net

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -6,9 +6,10 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/taxi?serverTimezone=UTC";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "1234";
+    private static final String URL =
+        "jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11510452?serverTimezone=UTC";
+    private static final String USERNAME = "sql11510452";
+    private static final String PASSWORD = "ZHjQ7eEAXI";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
